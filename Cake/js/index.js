ba65aae4525d84c64a5d2aa3b15c7046e2f4c597
XSeanU <51028862+hi-yiyang@users.noreply.github.com>
2022-09-26 00:04:34 +0400

    cake

--- Cake/js/index.js
+++ Cake/js/index.js
@@ -1,3 +1,6 @@
 // I am going to continue to improve this...
 // I attempt to perform candle again but could not overcome the previous version.
 // http://codepen.io/fixcl/pen/nKFDr
+
+var audio = document.getElementById("song");
+audio.volume = 0.2;
